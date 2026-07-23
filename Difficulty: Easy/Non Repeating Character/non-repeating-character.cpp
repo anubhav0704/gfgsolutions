e05f0ef061d3f998a88765bea5cf1cb6f528eda4
Anubhav Kumar <152191263+anubhav0704@users.noreply.github.com>
2026-07-23 15:42:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+    char nonRepeatingChar(string &s) {
+        //  code here
+        int n=s.size();
+        unordered_map<char,int> mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+        }
+        for (char c : s) {
+            if (mp[c] == 1)
+                return c;
+        }
+        return '$';
+    }
+};
